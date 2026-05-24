3c08688122b10e3bd7806457a13430e1913f28b1
estudosdanielsa-cmd <estudosdanielsa@gmail.com>
2026-05-24 20:52:36 +0000

    AJUSTE SUPABASE

---
+++ header.js
@@ -0,0 +1,32 @@
+// 1. CONFIGURAÇÃO: Insira aqui as chaves que você copiou do Supabase
+const SUPABASE_URL = "https://lnmbbziyesrgrzqxwvhp.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_Z_6qpZxu9JlFD-Wnqyz2DA_PQuXTBgN";
+
+     
+// 2. CONEXÃO: Cria o cliente oficial de comunicação com a nuvem
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// 3. LOGICA: Exemplo de como o botão "Dar Entrada" vai funcionar no Tablet
+async function darEntradaEstoque(idProduto, quantidadeInformada) {
+    // Busca o produto atual na tabela do Supabase
+    const { data: produto, error } = await supabase
+        .from('produtos')
+        .select('quantidade')
+        .eq('id', idProduto)
+        .single();
+
+    if (produto) {
+        // Faz a conta matemática: quantidade antiga + nova quantidade
+        const novaQuantidade = produto.quantidade + parseInt(quantidadeInformada);
+
+        // Atualiza o novo valor de volta na nuvem
+        await supabase
+            .from('produtos')
+            .update({ quantidade: novaQuantidade })
+            .eq('id', idProduto);
+            
+        alert("Entrada registrada com sucesso!");
+    } else {
+        alert("Produto não encontrado.");
+    }
+}
